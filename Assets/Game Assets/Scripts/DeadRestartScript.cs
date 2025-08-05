ed1ef0db25c21be3ebf88651f252a386f90334f7
Chakradhar <chakribathala@gmail.com>
2025-08-05 17:17:28 +0530

    Death After Falling From Platform

---
+++ Assets/Game Assets/Scripts/DeadRestartScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadRestartScript : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float followSpeed = 5f;
+    private Vector2 targetPosition;
+
+    void Update()
+    {
+        
+            // Keep current Y, match player’s X
+            targetPosition = new Vector2(playerTransform.position.x, transform.position.y);
+
+            // Smoothly move the platform to the target position
+            transform.position = Vector2.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+      
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("PlayerDead");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+            
+    }
+}
