4fb1519d7b206014f9529c50162a64720aa06451
Chakradhar <chakribathala@gmail.com>
2025-08-18 12:06:10 +0530

    Game Over UI with a Restart button on Player Death

--- Assets/Game Assets/Scripts/GameOverController.cs
+++ Assets/Game Assets/Scripts/GameOverController.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour
 {
-    public Button restartButton;
+    public Button restartButton;    
+    public Button MainMenuButton;    
 
     private void Awake()
     {
         restartButton.onClick.AddListener(ReloadLevel);
+        MainMenuButton.onClick.AddListener(MainMenu);
     }
     public void PlayerDied()
     {
         gameObject.SetActive(true);
     }
 
+    private void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     private void ReloadLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
